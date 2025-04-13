a8b5b1e726bb974c2e67aa12f20dcc05bda5e5d2
Parth1125 <pgera326@gmail.com>
2025-04-13 17:23:13 +0530

    feat: build chat interface and chat page

--- app/dashboard/chat/[chatId]/page.tsx
+++ app/dashboard/chat/[chatId]/page.tsx
@@ -1,7 +1,34 @@
+import ChatInterface from "@/components/ChatInterface";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { getConvexClient } from "@/lib/convex";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const ChatPage = () => {
-  return <div>ChatPage</div>;
+interface ChatPageProps {
+  params: Promise<{ chatId: Id<"chats"> }>;
+}
+const ChatPage = async ({ params }: ChatPageProps) => {
+  const { chatId } = await params;
+  const { userId } = await auth(); // using useauth because this is server side component id coming here is server side
+  const id = await auth();
+  console.log(id, "id");
+  if (!userId) {
+    redirect("/");
+  }
+  try {
+    const convex = getConvexClient();
+    const initialMessages = await convex.query(api.messages.list, { chatId });
+
+    return (
+      <div className="flex-1 overflow-hidden">
+        <ChatInterface chatId={chatId} initialMessages={initialMessages} />
+      </div>
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default ChatPage;
